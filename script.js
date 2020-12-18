59d287866d8070a0c12a977d921d61175d887e57
programmes-csiofx92 <pauldeaintalbin@gmx.fr>
2020-12-18 17:19:19 +0100

    Changement du menu déroulant

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const fermerAlerte = document.getElementById('fermerAlerte');
 fermerAlerte.addEventListener('click', function () {
     document.querySelector('.alert').style.display = "none";
 })
+document.querySelector('.jeux').addEventListener('mouseover', function () {
+    document.querySelector('.flecheje').innerHTML = "&#9650;";
+})
+document.querySelector('.jeux').addEventListener('mouseout', function () {
+    document.querySelector('.flecheje').innerHTML = "&#9660;";
+})
+document.querySelector('.sous').addEventListener('mouseover', function () {
+    document.querySelector('.flecheje').innerHTML = "&#9650;";
+})
+document.querySelector('.sous').addEventListener('mouseout', function () {
+    document.querySelector('.flecheje').innerHTML = "&#9660;";
+})
+document.querySelector('.dessin').addEventListener('mouseover', function () {
+    document.querySelector('.flechebl').innerHTML = "&#9650;";
+})
+document.querySelector('.dessin').addEventListener('mouseout', function () {
+    document.querySelector('.flechebl').innerHTML = "&#9660;";
+})
+document.querySelector('.souse').addEventListener('mouseover', function () {
+    document.querySelector('.flechebl').innerHTML = "&#9650;";
+})
+document.querySelector('.souse').addEventListener('mouseout', function () {
+    document.querySelector('.flechebl').innerHTML = "&#9660;";
+})
