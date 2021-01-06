ca5b8ec38a67269458d6f8d5d1532e5b66100dcd
programmes-csiofx92 <pauldeaintalbin@gmx.fr>
2021-01-06 13:48:06 +0100

    zrger

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ fermerAlerte.addEventListener('click', function () {
     document.querySelector('.alert').style.display = "none";
 })
 document.querySelector('.jeux').addEventListener('mouseover', function () {
-    document.querySelector('.flecheje').innerHTML = "&#9650;";
+    document.querySelector('.flecheje').innerHTML = "△";
 })
 document.querySelector('.jeux').addEventListener('mouseout', function () {
-    document.querySelector('.flecheje').innerHTML = "&#9660;";
+    document.querySelector('.flecheje').innerHTML = "▽";
 })
 document.querySelector('.sous').addEventListener('mouseover', function () {
-    document.querySelector('.flecheje').innerHTML = "&#9650;";
+    document.querySelector('.flecheje').innerHTML = "△";
 })
 document.querySelector('.sous').addEventListener('mouseout', function () {
-    document.querySelector('.flecheje').innerHTML = "&#9660;";
+    document.querySelector('.flecheje').innerHTML = "▽";
 })
 document.querySelector('.dessin').addEventListener('mouseover', function () {
-    document.querySelector('.flechebl').innerHTML = "&#9650;";
+    document.querySelector('.flechebl').innerHTML = "△";
 })
 document.querySelector('.dessin').addEventListener('mouseout', function () {
-    document.querySelector('.flechebl').innerHTML = "&#9660;";
+    document.querySelector('.flechebl').innerHTML = "▽";
 })
 document.querySelector('.souse').addEventListener('mouseover', function () {
-    document.querySelector('.flechebl').innerHTML = "&#9650;";
+    document.querySelector('.flechebl').innerHTML = "△";
 })
 document.querySelector('.souse').addEventListener('mouseout', function () {
-    document.querySelector('.flechebl').innerHTML = "&#9660;";
+    document.querySelector('.flechebl').innerHTML = "▽";
 })
